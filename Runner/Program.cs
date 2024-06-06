8b1f3d48168829ee31e21ed508be263bdf554c20
Park, Jonghyeon <belline0124@gmail.com>
2024-06-06 12:52:47 +0900

    Add Runner's receiving input function and handler

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -4,50 +4,82 @@ using QuineMcCluskey.Term;
 
 public class Program
 {
-    public static void Main(string[] args)
+    struct ProgramInputFormat
     {
-        QuineMcCluskeyWorker worker = new QuineMcCluskeyWorker(
-            new List<QMCTerm> {
-                new QMCTerm(4, new Bit[]{ Bit.F, Bit.F, Bit.F, Bit.F }, 0),  // A
-                new QMCTerm(4, new Bit[]{ Bit.F, Bit.T, Bit.F, Bit.F }, 4),  // B
-                new QMCTerm(4, new Bit[]{ Bit.T, Bit.F, Bit.F, Bit.F }, 8),  // C
-                new QMCTerm(4, new Bit[]{ Bit.F, Bit.T, Bit.F, Bit.T }, 5),  // D
-                new QMCTerm(4, new Bit[]{ Bit.T, Bit.T, Bit.F, Bit.F }, 12), // E
-                new QMCTerm(4, new Bit[]{ Bit.F, Bit.T, Bit.T, Bit.T }, 7),  // F
-                new QMCTerm(4, new Bit[]{ Bit.T, Bit.F, Bit.T, Bit.T }, 11), // G
-                new QMCTerm(4, new Bit[]{ Bit.T, Bit.T, Bit.T, Bit.T }, 15)  // H
-            }, 4, new string[] {"w", "x", "y", "z"}
-        );
-        Console.WriteLine("Before ===========");
-        Console.WriteLine(worker.ToString());
-        worker.Compute();
-        Console.WriteLine("After ============");
-        Console.WriteLine(worker.ToString());
-        Console.WriteLine("Prime Implicants");
-
-        SortedSet<QMCTerm> primeImplicants = worker.GetPrimeImplicants();
-        List<SortedSet<QMCTerm>> essentialPrimeImplicants = worker.FindEssentialPrimeImplicants();
-
-        foreach(var set in essentialPrimeImplicants)
-        {
-            Console.WriteLine(string.Join(", ", set));
-        }
-
-        Console.WriteLine("Result");
-        Console.WriteLine(string.Join("\n", worker.RenderMinSOP()));
+        public int argc;
+        public string[] argv;
+        public int[] minterms;
     }
 
-    static void ReceiveInput()
+    public static void Main(string[] args)
     {
+        ProgramInputFormat inputFormat = ReceiveInput();
+        List<QMCTerm> qmcTerms = new List<QMCTerm>();
+        foreach(int termId in inputFormat.minterms)
+        {
+            Bit[] newBits = new Bit[inputFormat.argc];
+            int n = termId;
+            for (int i = 0; i < inputFormat.argc; i++)
+            {
+                newBits[inputFormat.argc - i - 1] = (n % 2 == 1) ? Bit.T : Bit.F;
+                n /= 2;
+            }
+
+            qmcTerms.Add
+            (
+                new QMCTerm
+                (
+                    inputFormat.argc,
+                    newBits,
+                    new int[]{ termId }
+                )
+            );
+        }
+
+        QuineMcCluskeyWorker worker = new QuineMcCluskeyWorker(
+            qmcTerms, inputFormat.argc, inputFormat.argv
+        );
+
+        Console.WriteLine("Essential Prime Implicants");
+        List<SortedSet<QMCTerm>> epiList = worker.FindEssentialPrimeImplicants();
+        Console.WriteLine
+        (
+            string.Join(
+                ", ",
+                epiList[0]
+                    .Select(x => x.ToVariables(worker.variableExpressions))
+            )
+        );
+        Console.WriteLine("");
+
+        Console.WriteLine("Minimum SOP");
+        List<string> minimumSOPs = worker.RenderMinSOP();
+        Console.WriteLine(string.Join("\n", minimumSOPs));
+    }
+
+
+    static ProgramInputFormat ReceiveInput()
+    {
+        ProgramInputFormat inputFormat = new ProgramInputFormat();
         Console.Write("Enter the number of variables: ");
-        int n = int.Parse(Console.ReadLine());
-        Console.Write("Enter the variables: ");
-        string[] variables = Console.ReadLine().Split(' ');
-        Console.Write("Enter the minterms: ");
-        int[] minterms = (
-            from x in Console.ReadLine().Split(' ')
-            select int.Parse(x)
-        ).ToArray();
+        inputFormat.argc = int.Parse(Console.ReadLine());
+        while (true)
+        {
+            Console.Write("Enter the variables (must be seperated by space(' ')): ");
+            string[] argv = Console.ReadLine().Split(' ');
+            if (argv.Length == inputFormat.argc)
+            {
+                inputFormat.argv = argv;
+                break;
+            }
+            Console.WriteLine($"Error! Length of variables({argv.Length} is not number of variables that entered ({inputFormat.argc}))");
+        }
+        
+        Console.Write("Enter the minterms (must be seperated by space(' ')): ");
+        inputFormat.minterms = Console.ReadLine().Split(' ')
+            .Select(x => int.Parse(x))
+            .ToArray();
+        return inputFormat;
     }
 
 }
