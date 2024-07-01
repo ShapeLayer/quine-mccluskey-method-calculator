a4bb103ca597926fc1c622419905939c6c2614b1
Park, Jonghyeon <belline0124@gmail.com>
2024-07-01 18:37:17 +0900

    Update QuineMcCluskeyTest.cs

--- QuineMcCluskeyTest/tests/QuineMcCluskeyTest.cs
+++ QuineMcCluskeyTest/tests/QuineMcCluskeyTest.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using QuineMcCluskey;
 using QuineMcCluskey.Commons;
+using QuineMcCluskey.Term;
 namespace QuineMcCluskeyTest;
 
 [TestClass]
